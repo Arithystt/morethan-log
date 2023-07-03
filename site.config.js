641fae9cd31eb6e6341d816472444684e24d323f
Arithystt <138445541+Arithystt@users.noreply.github.com>
2023-07-03 18:59:52 +0800

    site.config.js

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://arithyst.vercel.app/",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://cdn.discordapp.com/attachments/875561648851681300/1125370450239823912/avatar.png", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "/avatar.png", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
   },
