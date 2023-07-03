15f221ef5e343d0974652ad8ac201d12890fb680
Arithystt <138445541+Arithystt@users.noreply.github.com>
2023-07-03 19:25:11 +0800

    site.config.js

--- site.config.js
+++ site.config.js
@@ -5,11 +5,15 @@ const CONFIG = {
     image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "ProRes/Master Music Video Collector",
     bio: "ProRes/Masters Music Video Collection",
-
+    email: "",
+    linkedin: "",
+    github: "",
+    instagram: "",
   },
   projects: [
     {
-
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
