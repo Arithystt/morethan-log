f7e3c3e0401257c5e80526f15575f405b72929bd
Arithystt <138445541+Arithystt@users.noreply.github.com>
2023-07-03 17:08:12 +0800

    site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Arithyst",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "arithystt@gmail.com",
+    Discord: "moonlight_18",
+    github: "Arithystt",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `Arithyst`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Arithyst",
+    description: "Pro-Res/Master Music Video Collection!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
@@ -51,7 +51,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
