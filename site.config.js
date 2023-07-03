a38c7dc9710f0264254a5c86a7251acd1d113042
Arithystt <138445541+Arithystt@users.noreply.github.com>
2023-07-03 18:12:54 +0800

    site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Arithyst",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "ProRes/Master Music Video Collector",
     bio: "ProRes/Masters Music Video Collection",
     email: "arithystt@gmail.com",
